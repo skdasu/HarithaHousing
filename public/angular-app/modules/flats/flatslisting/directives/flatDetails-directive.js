a4dc4a98a6e29d76b694c8d4012bc107bd78349f
skdasu <skdasu@gmail.com>
2016-12-30 07:55:05 +0530

    Finalising flats directive

--- public/angular-app/modules/flats/flatslisting/directives/flatDetails-directive.js
+++ public/angular-app/modules/flats/flatslisting/directives/flatDetails-directive.js
@@ -1,67 +1,37 @@
-// find the already delcared module and tag the directive
-angular.module('harithaHousing').
-directive('flatDetails', ['$state', '$rootScope', function($state, $rootScope) {
-
-    var linkFunction = function($scope, elem, attrs) {
-        //console.log($scope.salesdetailsdata);
-        $scope.flatsDetailsData = JSON.parse($scope.flatsdetailsdata);
-        $scope.flatsDetails = $scope.flatsDetailsData;
-        $scope.testmsg = "test1234"
-    };
-
-    return {
-        // can be used as attribute or element
-        restrict: 'AE',
-        templateUrl: "angular-app/modules/flats/flatslisting/directives/flatDetails-template.html",
-        link: linkFunction,
-        scope: {
-            flatsdetailsdata: "="
-        }
-    };
-}]);
-
-
-
-/*
-// find the already delcared module and tag the directive
 angular.module('harithaHousing').
 directive('flatDetails', function() {
 
-    var linkFunction = function($scope, elem, attrs) {
-        // pre: function(scope, elem, attr) {
-        //     console.log("pre linkFunction");
-        //
-        // };
-        // post: function(scope, elem, attr) {
-        //     console.log("post linkFunction");
-        // };
-        console.log("linkFunction");
-        $scope.flatsDetailsData = JSON.parse($scope.flatsdetailsdata);
-        $scope.flatsDetails = $scope.flatsDetailsData;
+    var linkFunction = function(scope, element, attrs) {
+        // observe changes in attribute - could also be scope.$watch
+        /*attrs.$observe('yourDirective', function (value) {
+            if (value) {
+                console.log(value);
+                // pass value to app controller
+                scope.variable = value;
+            }
+        });*/
     };
 
-    var controllerFunction = function(scope) {
-        console.log("controllerFunction");
-        $scope.flatsDetailsData = JSON.parse($scope.flatsdetailsdata);
-        $scope.flatsDetails = $scope.flatsDetailsData;
-};
+    var controllerFunction = function($scope, $element, $attrs) {
+        // observe changes in attribute - could also be scope.$watch
+        var unRegissterWatch = $scope.$watch('inData', function(newValue, oldValue) {
+            //console.log('hey, myVar has changed!'+ angular.toJson(newValue));
+            if (newValue.flatListing) {
+                console.log(newValue);
+                $scope.flatsDetails = newValue.flatListing;
+                unRegissterWatch();
+                //setData();
+            }
+        });
+    };
 
-// var prelinkFunc = function(scope, elem, attr) {
-//     console.log("pre linkFunction");
-// };
-//
-// var postlinkFunc = function(scope, elem, attr) {
-//     console.log("post linkFunction");
-// };
-
-return {
-    // can be used as attribute or element
-    restrict: 'AE',
-    templateUrl: "angular-app/modules/flats/flatslisting/directives/flatDetails-template.html",
-    link: linkFunction,
-    controller: controllerFunction,
-    scope: {
-        flatsdetailsdata: "@"
-    }
-};
-}); */
+    return {
+        restrict: 'E',
+        templateUrl: "angular-app/modules/flats/flatslisting/directives/flatDetails-template.html",
+        link: linkFunction,
+        controller: controllerFunction,
+        scope: {
+            inData: "="
+        }
+    };
+});
